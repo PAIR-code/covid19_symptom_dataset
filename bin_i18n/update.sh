a23c7fe953874523c81c8ea6cd88b8d787fd329a
Adam Pearce <adampearce@google.com>
2021-01-05 10:34:04 -0500

    quiet gsutil

--- bin_i18n/update.sh
+++ bin_i18n/update.sh
@@ -6,7 +6,7 @@ mkdir -p ../data-raw/i18n/
 mkdir -p ../data-parsed/i18n/
 
 # download files from GCS
-gsutil -m rsync -d -r gs://gcs-public-data---symptom-search ../data-raw/i18n/
+gsutil -m -q rsync -d -r gs://gcs-public-data---symptom-search ../data-raw/i18n/
 
 
 node calc-download-list.js
@@ -14,4 +14,4 @@ node calc-download-list.js
 node parse.js
 
 # upload to GCS
-gsutil -m rsync -r ../data-parsed/i18n gs://uncertainty-over-space/ssd_i18n
+gsutil -m -q rsync -r ../data-parsed/i18n gs://uncertainty-over-space/ssd_i18n
